9665d9b9ad05f7da51c32d4a70875ac47c2f2bb2
XTxiaoting14332 <HT30332@126.com>
2025-10-03 22:12:47 +0800

    🏷️v0.1.8

--- RELEASE_CHANGELOG.md
+++ RELEASE_CHANGELOG.md
@@ -1,4 +1,3 @@
-# 🌈 Changelog 2025.09.13 🔧
+# 🌈 Changelog 2025.10.03 🔧
 by @XTxiaoting14332
-默认监听127.0.0.1以增强安全性
-首次启动自动设置随机Token
+新增stderr页面查询
--- utils/core.dart
+++ utils/core.dart
@@ -10,7 +10,7 @@ import 'package:uuid/uuid.dart';
 class AgentMain {
   /// 软件版本
   static String version() {
-    return '0.1.7';
+    return '0.1.8';
   }
 
   /// 初始化用户配置文件
